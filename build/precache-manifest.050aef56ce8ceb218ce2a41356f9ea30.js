7e56d0baecf0770685defc388132f18da9e5826d
viamax <viamax@gmail.com>
2020-10-08 20:18:08 +0300

    delpoy build

---
+++ build/precache-manifest.050aef56ce8ceb218ce2a41356f9ea30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523fccc149c54c381c61fbde05336bb9",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "8f6c96c58578023c8a1b",
+    "url": "/money/static/css/main.c0f79b1d.chunk.css"
+  },
+  {
+    "revision": "09fea4a066587b72b7e5",
+    "url": "/money/static/js/2.8718a149.chunk.js"
+  },
+  {
+    "revision": "21838616f74ece053dfb7eff1245e0f5",
+    "url": "/money/static/js/2.8718a149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6c96c58578023c8a1b",
+    "url": "/money/static/js/main.89f86af5.chunk.js"
+  },
+  {
+    "revision": "b69c5d245c6ae4a9c391",
+    "url": "/money/static/js/runtime-main.6ec5249a.js"
+  }
+]);
