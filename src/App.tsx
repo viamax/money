368b4648f2a0191550acccce0fd4b8c7c64ea836
viamax <viamax@gmail.com>
2020-10-08 22:32:28 +0300

    change route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/pages/navigation/navigation";
-import {SignInPage} from "./components/pages/signin/signInPage";
+import { SignInPage } from "./components/pages/signin/signInPage";
 import * as ROUTES from "./constants/routes";
 import SignUpPage from "./components/pages/signup/signUpPage";
-import {createBrowserHistory} from "history";
-import {withAuthentication} from "./components/Session";
-import {HomePage} from "./components/pages/homePage/homePage";
+import { createBrowserHistory } from "history";
+import { withAuthentication } from "./components/Session";
+import { HomePage } from "./components/pages/homePage/homePage";
 
 //region [[ Props ]]
 
@@ -15,23 +15,18 @@ export interface AppProps {}
 
 //endregion [[ Props ]]
 
-const historyGlobal = createBrowserHistory({forceRefresh:true})
+const historyGlobal = createBrowserHistory({ forceRefresh: true });
 
 export const App = ({ ...props }: AppProps) => {
-
-
-
   return (
     <>
       <Router history={historyGlobal}>
         <div>
           <Navigation history={historyGlobal} />
 
-
           <Route path={ROUTES.SIGN_IN} component={SignInPage} />
           <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-
+          <Route path={"/"} component={HomePage} />
 
           {/*
 
