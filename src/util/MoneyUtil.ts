324a14e208c580dea13154a789762bf146b4d52c
viamax <viamax@gmail.com>
2020-10-08 20:31:30 +0300

    test

--- src/util/MoneyUtil.ts
+++ src/util/MoneyUtil.ts
@@ -4,7 +4,7 @@ import {Transaction, TransactionOld} from '../model/transaction';
 
 export class MoneyUtil {
     static numberWithCommas(x) {
-
+console.log("max");
         return x ? x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : "";
     }
 }
